e93cbc08db18117d54401e4d89850604bc1dad2b
lx_along <huyi99319@163.com>
2021-04-05 18:15:44 +0800

    commit config 20210405

--- config/index.js
+++ config/index.js
@@ -6,14 +6,14 @@ const path = require('path')
 
 module.exports = {
   dev: {
-    // Paths【配置代理】
+    // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      '/api':{
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        pathRewrite:{
+      '/api':{   //匹配所有以 '/api' 开头的请求路径
+        target: 'http://localhost:3000', //代理目标的基础路径
+        changeOrigin: true,  //支持跨域
+        pathRewrite:{   //重写路径，去掉路径中开头的 '/api'
           '^/api': ''
         }
       }
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
-<!--
-     应用根组件vue
--->
 <template>
-  <div>
-    <router-view/>
-    <!--3、使用组件-->
+  <div id="app">
+    <router-view />
     <!--控制Footer的显示隐藏-->
     <FooterGuide v-show="$route.meta.showFooter"/>
   </div>
@@ -13,19 +9,18 @@
 <script>
   /*引入FooterGuide组件*/
   import FooterGuide from './components/FooterGuide/FooterGuide'
-  import {reqCategorys} from './api'
+  import  {reqCategorys} from './api'
 
   export default {
-    /*2、定义为组件标签*/
     components:{
       FooterGuide
     },
-    //测试异步获取数据
-    async mounted(){
+//测试异步获取数据
+    async mounted () {
       const result = await reqCategorys();
       console.log(result);
     }
-}
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
